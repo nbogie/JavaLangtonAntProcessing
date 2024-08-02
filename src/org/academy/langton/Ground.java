0fd592dec94e183869625a583235ad8f16ab2e28
nbogie <neillbogie@googlemail.com>
2024-08-02 11:36:48 +0100

    simplify Ground#draw()

--- src/org/academy/langton/Ground.java
+++ src/org/academy/langton/Ground.java
@@ -58,26 +58,18 @@ public class Ground {
         return new PixelPosition(cellSize * cellPos.x(), cellSize * cellPos.y());
     }
 
-    public static void drawSquareAtGridPosition(GridPosition pos, PApplet applet, int cellSize){
+    public static void drawSquareAtGridPosition(GridPosition pos, PApplet applet, int cellSize) {
         PixelPosition pixelPosition = cellPositionAsPixelPosition(pos, cellSize);
         applet.square(pixelPosition.x(), pixelPosition.y(), cellSize);
     }
+
     void draw() {
+        applet.stroke(100);
 
         for (List<Cell> rowOfCells : rowsOfCells) {
             for (Cell cell : rowOfCells) {
-                applet.stroke(100);
-
                 applet.fill(cell.isActive() ? 40 : 255);
                 Ground.drawSquareAtGridPosition(cell.gridPosition(), applet, cellSize);
-
-
-                if (cell.isActive()) {
-                    applet.textSize(cellSize/2f);
-                    applet.fill(255, 0, 0);
-
-                }
-
             }
         }
     }
